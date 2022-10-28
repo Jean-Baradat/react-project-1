f83a815cef2596a251781308dcdba35ab67312f4
Ovhas <dovakinbaradat@gmail.com>
2022-10-28 18:33:31 +0200

    Basics of the project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"
-import About from "./pages/About"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navigation from "./components/Navigation";
+import Logo from "./components/Logo"
 
 const App = () => {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Home />} />
-        </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <>
+            <Logo />
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<Home />} />
+            </Routes>
+        </>
+    );
 };
 
 export default App;
